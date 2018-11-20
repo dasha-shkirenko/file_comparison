092ed6e7b180d08022df349d25b256c8719c2955
Daria Shkirenko <daria.shkirenko@gmail.com>
2018-11-20 18:58:57 +0200

    Add third argument allows to ignore values in specified column

--- comparison.py
+++ comparison.py
@@ -4,7 +4,7 @@ import xlrd
 
 # 676
 
-def compare_reports(f1, f2):
+def compare_reports(f1, f2, ignore_column = 1000000):
     file1 = xlrd.open_workbook(f1)
     file2 = xlrd.open_workbook(f2)
 
@@ -20,15 +20,19 @@ def compare_reports(f1, f2):
                 sheet2.ncols,
             ))
             break
+
         if rownum < sheet1.nrows:
             row_rb1 = sheet1.row_values(rownum)
             row_rb2 = sheet2.row_values(rownum)
 
             for colnum, (c1, c2) in enumerate(izip_longest(row_rb1, row_rb2)):
                 if c1 != c2:
+                    if str(colnum + 1) == str(ignore_column):
+                        break
                     print(
-                        "Row: {} Col: '{}' : \n {}: {} \n {}: {} \n".format(
+                        "Row #{}, Col #{}, Col_name: '{}' : \n {}: {} \n {}: {} \n".format(
                             rownum + 1,
+                            colnum + 1,
                             sheet1.cell_value(0, colnum),
                             f1,
                             c1,
@@ -39,10 +43,8 @@ def compare_reports(f1, f2):
                     if sheet1.nrows != sheet2.nrows:
                         print('From row #{} reports start differentiating.'.format(sheet1.nrows))
                         return
-        else:
-            print("Row {} missing".format(rownum + 1))
 
 
 if __name__ == '__main__':
-    _, f1, f2 = sys.argv
-    compare_reports(f1, f2)
+    _, f1, f2, ignore_column = sys.argv
+    compare_reports(f1, f2, ignore_column)
